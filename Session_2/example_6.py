4a969cff2a1a04c920b6b8a4725f1a78987ed365
hadi tayanloo <htayanloo@gmail.com>
2023-06-26 23:47:04 +0330

    session_2_part_2

---
+++ Session_2/example_6.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+list_ip = ["192.168.0.1","192.168.0.2","192.168.0.5"]
+# print("host %s" % list_ip[0])
+# print("host %s" % list_ip[1])
+
+# print("1")
+# print("2")
+# print("3")
+
+
+
+# for a in range(1,11):
+#     print(a)
+
+
+
+# temp = [5,3,6,3,2,3,5,7]
+# print(temp)
+# temp.sort()
+# print(temp)
+# temp.sort(reverse=True)
+# print(temp)
+
+
+# -----------
+
+list_a = [1,2,3,4,5,6]
+list_b = [7,8,9,10,11,12]
+list_c = list_b + list_a
+# print(list_c)
+
+for item in list_b:
+    list_a.append(item)
+print(list_a)
+
+
+# ------
+
+
+
--- Session_2/main.py
+++ Session_2/main.py
@@ -1,2 +1,58 @@
 #!/usr/bin/python
 
+temp_list_1 = ["111","2222"]
+temp_list_2 = [1111,22222]
+temp_list_3 = [True,False]
+
+
+temp_list_4 = ["111",111,True]
+
+
+
+# print(temp_list_4[:2])
+
+temp_user = temp_list_4[1]
+
+# temp_list_4[0] = "BBBB","AAAA"
+
+
+temp_list_4[0:2] = ["BBBB"]
+
+#
+# temp_list_4[0:1] = ["BBBB","AAAA"]
+
+temp_list_5 = []
+
+# temp_list_5.append("admin")
+# print(temp_list_5)
+# temp_list_5.append("admin2")
+# print(temp_list_5)
+# temp_list_5.insert(0,"admin1")
+# print(temp_list_5)
+# temp_list_5.remove("admin2")
+# print(temp_list_5)
+# ---------------------------------------
+#
+# temp_list_6 = ["111",111,True]
+# print(temp_list_6)
+# print(temp_list_6.pop(0))
+# print(temp_list_6)
+
+# ------
+#
+# temp_list_7 = ["111",111,True]
+#
+# del temp_list_7[0]
+# print(temp_list_7)
+
+
+# -------
+#
+# temp_list_9 = ["111",111,True]
+#
+# temp_list_9.clear()
+# print(temp_list_9)
+
+
+
+
